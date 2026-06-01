4e7443f68cef722e8d7ac3112cf0489aa5bc76a6
senaleo89 <senaleo89@gmail.com>
2026-06-01 20:58:20 -0300

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,11 @@ import os
 TOKEN = os.getenv("TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 API_KEY = os.getenv("API_KEY")
+
+LIGAS_PERMITIDAS = [
+    71,   # Brasileirão Série A
+    72,   # Brasileirão Série B
+    39,   # Premier League
+    140,  # La Liga
+    78    # Bundesliga
+]
