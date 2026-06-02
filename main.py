6f7e857333cb5b76a1f6fd61f7a48bb3b7827bfa
senaleo89 <senaleo89@gmail.com>
2026-06-01 21:49:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ from telegram import Bot
 from config import TOKEN, CHAT_ID
 import time
 
-bot = Bot(token=TOKEN)
+print("TOKEN existe?", TOKEN is not None)
+print("CHAT_ID:", CHAT_ID)
 
-bot.send_message(
-    chat_id=CHAT_ID,
-    text="✅ TESTE FINAL FUNCIONANDO!"
-)
-
-print("Mensagem enviada")
+try:
+    bot = Bot(token=TOKEN)
+    bot.send_message(
+        chat_id=CHAT_ID,
+        text="✅ Teste Telegram funcionando!"
+    )
+    print("Mensagem enviada com sucesso")
+except Exception as erro:
+    print("ERRO AO ENVIAR:", erro)
 
 while True:
     time.sleep(60)
