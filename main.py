9a81a053a8b87f080f0f9bc79e7fe44e6657a074
senaleo89 <senaleo89@gmail.com>
2026-06-01 21:44:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from telegram import Bot
 from config import TOKEN, CHAT_ID
+import time
 
 bot = Bot(token=TOKEN)
 
 bot.send_message(
     chat_id=CHAT_ID,
-    text="✅ TESTE DEFINITIVO: Railway conectado ao Telegram!"
+    text="✅ TESTE FINAL FUNCIONANDO!"
 )
 
-print("Mensagem enviada com sucesso")
+print("Mensagem enviada")
+
+while True:
+    time.sleep(60)
